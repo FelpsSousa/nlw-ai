40e060eb7ea65f49670cc56860b55c3c359e95d0
FelpsSousa <felipeluis.felpssousa@gmail.com>
2023-09-17 06:39:32 -0300

    Criando e separando main e divs e demais subespaços. Adicionando o componente Textarea do Shadcn UI e aplicando configurações default essenciais para nossa aplicação.

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,9 +1,11 @@
 import { Github } from 'lucide-react'
 import { Button } from "./components/ui/button";
+import { Separator } from '@radix-ui/react-separator';
+import { Textarea } from './components/ui/textarea';
 
 export function App() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
         <h1 className="text-xl font-bold">upload.ai</h1>
 
@@ -11,12 +13,37 @@ export function App() {
           <span className="text-sm text-muted-foreground">
             Desenvolvido com ❤ no NLW da Rocketseat
           </span>
+
+          <Separator orientation='vertical' className='h-6' />
+
           <Button variant={"outline"}>
             <Github className="w-4 h-4 mr-2" />
             GitHub
           </Button>
         </div>
       </div>
+
+      <main className='flex-1 p-6 flex gap-6'>
+        <div className='flex flex-col flex-1 gap-4'>
+          <div className='grid grid-rows-2 gap-4 flex-1'>
+            <Textarea 
+              className='resize-none p-4 leading-relaxed'
+              placeholder='Inclua o prompt para a IA...'
+            />
+            <Textarea 
+              className='resize-none p-4 leading-relaxed'
+              placeholder='Resultado gerado pela IA...'
+              readOnly
+            />
+          </div>
+
+          <p className='text-sm text-muted-foreground'>
+            Lembre-se: você pode utilizar a variável <code className='text-violet-400'>{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.
+          </p>
+        </div>
+        <aside className='w-80'></aside>
+      </main>
+      
     </div>
   )
 }
