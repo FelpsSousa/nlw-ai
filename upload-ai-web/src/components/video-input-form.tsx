9fb4523e2198c9892fbb5f4848fa1c0aea67e6df
FelpsSousa <felipeluis.felpssousa@gmail.com>
2023-09-23 10:40:48 -0300

    Configuradas as bibliotecas e dependências FFmpeg para nossa aplicação.

--- upload-ai-web/src/components/video-input-form.tsx
+++ upload-ai-web/src/components/video-input-form.tsx
@@ -3,11 +3,12 @@ import { Separator } from "./ui/separator";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
-import { ChangeEvent, useMemo, useState } from "react";
+import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
 
 export function VideoInputForm() {
 
   const [videoFile, setVideoFile] = useState<File | null>(null)
+  const promptInputRef = useRef<HTMLTextAreaElement>(null)
 
   function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
     const { files } = event.currentTarget
@@ -21,6 +22,19 @@ export function VideoInputForm() {
     setVideoFile(selectedFile)
   }
 
+  function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const prompt = promptInputRef.current?.value
+
+    if (!videoFile) {
+      return
+    }
+
+    // converter o vídeo em áudio
+    
+  }
+
   const previewURL = useMemo(() => {
     if (!videoFile) {
       return null
@@ -30,7 +44,7 @@ export function VideoInputForm() {
   }, [videoFile])
 
   return (
-    <form className='space-y-6'>
+    <form onSubmit={handleUploadVideo} className='space-y-6'>
       <label 
         htmlFor="video"
         className='relative border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center hover:bg-primary/5'
@@ -52,6 +66,7 @@ export function VideoInputForm() {
       <div className='space-y-2'>
         <Label htmlFor='transcription_prompt'>Prompt de transcrição</Label>
         <Textarea
+          ref={promptInputRef}
           id='transcription_prompt'
           className='h-20 leading-relaxed resize-none'
           placeholder='Inclua palavras-chave mencionadas no vídeo separadas por vírgula ( , )'
